752c07f8fe5621a3b3566a13e3bd2708a2be0113
Ignacio Castro <ignaciocastro347@gmail.com>
2022-10-11 20:15:20 -0300

    3. BaseModel3

--- models/base_model.py
+++ models/base_model.py
@@ -10,10 +10,10 @@ class BaseModel:
         self.created_at = self.updated_at = datetime.now()
 
     def to_dict(self):
-        return { **self.__dict__, __class__: type(self).__name__ }
+        return { **self, __class__: type(self).__name__ }
 
     def __str__(self):
-        return "[BaseModel] ({}) {}".format(self.id, self)
+        return "[BaseModel] ({}) {}".format(self.id, self.__dict__)
 
     def save(self):
         self.updated_at = datetime.now()
