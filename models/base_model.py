da27988d9f39466029cb0f7a53a1869f3354b290
Ignacio Castro <ignaciocastro347@gmail.com>
2022-10-11 20:12:05 -0300

    3. BaseModel3

--- models/base_model.py
+++ models/base_model.py
@@ -7,13 +7,13 @@ from datetime import datetime
 class BaseModel:
     def __init__(self):
         self.id = str(uuid.uuid4())
-        self.created_at = self.updated_at = str(datetime.now())
+        self.created_at = self.updated_at = datetime.now()
+
+    def to_dict(self):
+        return { **self.__dict__, __class__: type(self).__name__ }
 
     def __str__(self):
         return "[BaseModel] ({}) {}".format(self.id, self.__dict__)
 
     def save(self):
-        self.updated_at = str(datetime.now())
-
-    def to_dict(self):
-        return { **self.__dict__, __class__: type(self).__name__ }
+        self.updated_at = datetime.now()
