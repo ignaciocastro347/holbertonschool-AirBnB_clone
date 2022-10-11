0ecf3d29651d004a64270368eb2c2f2b8688c0a6
root <root@DESKTOP-AJ89OOU.localdomain>
2022-10-11 20:06:13 -0300

    3. BaseModel3

---
+++ models/base_model.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Base model """
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = self.updated_at = str(datetime.now())
+
+    def __str__(self):
+        return "[BaseModel] ({}) {}".format(self.id, self.__dict__)
+
+    def save(self):
+        self.updated_at = str(datetime.now())
+
+    def to_dict(self):
+        return { **self.__dict__, __class__: type(self).__name__ }
