10ba4037c6bc3ecc9ee6c3c54b3e57ffd1fd89a7
EmilianoJZX100 <5083@holbertonstudents.com>
2022-10-12 16:16:45 -0300

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -10,7 +10,7 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Arguments"""
-        if len(kwargs) != 0:
+        if len(kwargs) == 0:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
         else:
