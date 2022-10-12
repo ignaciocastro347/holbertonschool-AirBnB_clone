358f61e36dc11cd67ff8d415d65a643fc1e33ead
EmilianoJZX100 <5083@holbertonstudents.com>
2022-10-12 16:11:54 -0300

    create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
-""" Base model """
+"""Base Model"""
+
 import uuid
 from datetime import datetime
 
 
 class BaseModel:
+    """Base Model Class"""
+
     def __init__(self, *args, **kwargs):
+        """Arguments"""
         if len(kwargs) != 0:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
         else:
-            for id in kwargs:
-                if id != "__class__":
-                    if id == "created_at" or id == "updated_at":
-                        i = kwargs[id]
+            for key in kwargs:
+                if key != "__class__":
+                    if key == "created_at" or id == "updated_at":
+                        i = kwargs[key]
                         datetime.strptime(i, "%Y-%m-%dT%H:%M:%S.%f")
-                    setattr(self, id, kwargs[id])
+                    setattr(self, key, kwargs[key])
 
     def to_dict(self):
+        """Return a Dictionary"""
         return { 
             **self.__dict__,
             "__class__": type(self).__name__,
@@ -26,7 +31,9 @@ class BaseModel:
         }
 
     def __str__(self):
+        """Prints string representation of class"""
         return "[BaseModel] ({}) {}".format(self.id, self.__dict__)
 
     def save(self):
+        """update instance of class"""
         self.updated_at = datetime.now()
