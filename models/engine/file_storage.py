65f00c04c48542aa1e275c2925a7559c505b4f40
Ignacio Castro <ignaciocastro347@gmail.com>
2022-10-16 17:49:28 -0300

    5. Store first object

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,33 +2,32 @@
 """files to json"""
 
 
-from models.base_model import BaseModel
 import json
 import os.path
 
 class FileStorage:
     """serializes to JSON and deserializes from JSON"""
-
-    def __init__(self):
-        self.__file_path = "file.json"
-        self.__objects = {}
+    __file_path = "file.json"
+    __objects = {}
 
     def all(self):
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
-        key = f"{obj.__class__.__name__}.{obj.id}"
-        self.__objects[key] = obj
+        FileStorage.__objects[obj.id] = obj
 
     def save(self):
         save_object = {}
-        for key in self.__objects:
-            save_object[key] = self.__objects[key].to_dict()
-        with open(self.__file_path, "w", encoding='utf-8') as file:
+        for key in FileStorage.__objects:
+            save_object[key] = FileStorage.__objects[key].to_dict()
+        with open(FileStorage.__file_path, "w", encoding='utf-8') as file:
             file.write(json.dumps(save_object))
 
     def reload(self):
-        if os.path.exists(self.__file_path):
-
-            with open(self.__file_path, "r", encoding="utf-8") as file:
-                self.__objects = json.load(file)
+        FileStorage.__objects = {}
+        if os.path.exists(FileStorage.__file_path):
+            with open(FileStorage.__file_path, "r", encoding="utf-8") as file:
+                json_content = file.read()
+            json_content = json.loads(json_content) if json_content is not None else []
+            for key, value in json_content.items():
+                FileStorage.__objects[key] = value
