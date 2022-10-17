c181b1a092db34593de31b9b8f0498e17cd9928b
EmilianoJZX100 <5083@holbertonschool.com>
2022-10-17 18:41:47 -0300

    test base model fix

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -13,5 +13,5 @@ class TestBaseModel(unittest.TestCase):
         self.assertTrue(old_updated_at != self.b1.updated_at)
 
     def test_to_dict(self):
-        self.assertTrue(self.b1.to_dict(), self.b1.to_dictionary())
+        self.assertTrue(self.b1.to_dict(), self.b1.to_dict())
 		
