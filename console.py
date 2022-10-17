b385153a9c639a9957c71c1d7d7a7face5689557
Ignacio Castro <ignaciocastro347@gmail.com>
2022-10-16 23:17:07 -0300

    6. Console 0.0.1

---
+++ console.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""files to json"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """HBNBCommand Class"""
+    prompt = '(hbnb)'
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        exit()
+
+    def do_EOF(self, arg):
+        """Quit command to exit the program"""
+        exit()
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
