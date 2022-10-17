e6fc5eb1fc0fedae02ad726eb84fbb792c1c43f1
EmilianoJZX100 <5083@holbertonschool.com>
2022-10-17 17:25:47 -0300

    9. More Classes test files

---
+++ tests/test_models/test_review.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Unittest for Review class """
+
+import unittest
+from models.review import Review
+
+class TestReview(unittest.TestCase):
+    def test_review(self):
+        model = Review()
+        self.assertTrue(type(mode.name) is str)
+
+if __name__ == ""__main__"":
+    unittest.main()
---
+++ tests/test_models/test_state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Unittest for State class """
+
+import unittest
+from models.state import State
+
+class TestState(unittest.TestCase):
+    def test_state(self):
+        model = State()
+        self.assertTrue(type(mode.name) is str)
+
+if __name__ == ""__main__"":
+    unittest.main()
+    
